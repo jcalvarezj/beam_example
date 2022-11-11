e36f9d5659b5cf5e0dd5989cab32b0233319ab3f
jcalvarezj <jcalvarezj@gmail.com>
2022-11-11 15:39:13 -0500

    Sink results to MongoDB and add transformations

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 A small sketchpad for Apache Beam pipelines with Python
 
-It uses the [Belarus Used Cars Prices](https://www.kaggle.com/datasets/slavapasedko/belarus-used-cars-prices) Kaggle dataset to perform transform operations on its data
+It uses the [Belarus Used Cars Prices](https://www.kaggle.com/datasets/slavapasedko/belarus-used-cars-prices) Kaggle dataset to perform transform operations on its data and load it onto a MongoDB database
 
 ## Execution
 
 Create a virtual environment and install the `apache_beam` dependency with PIP
+
+Set the `DB_USER`, `DB_PASSWORD`, and `DB_HOST` environment variables
 
 Run the `main.py` script
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import apache_beam as beam
+import os, apache_beam as beam
+from apache_beam.io.mongodbio import WriteToMongoDB
 
 
-MAKE_IDX = 0
-PRICE_IDX = 2
+col_indexes = {
+    "make": 0,
+    "model": 1,
+    "price": 2,
+    "year": 3,
+    "condition": 4,
+    "mileage": 5,
+    "fuel_type": 6,
+    "volume": 7,
+    "color": 8,
+    "transmission": 9,
+    "drive_unit": 10,
+    "segment": 11
+}
 
 
 class FilterOutHeader(beam.DoFn):
@@ -24,20 +37,72 @@ def dehyphenate(record):
     """
         Removes hyphens of a record's make field, replacing them with spaces
     """
-    record[MAKE_IDX] = record[MAKE_IDX].replace("-", " ")
+    record["make"] = record["make"].replace("-", " ")
+    return record
+
+
+def convert_to_dict(line_fields):
+    """
+        Transforms the line_fields element to a dictionary
+    """
+    return {
+        "make": line_fields[col_indexes["make"]],
+        "model": line_fields[col_indexes["model"]],
+        "price": float(line_fields[col_indexes["price"]]),
+        "year": int(line_fields[col_indexes["year"]]),
+        "condition": line_fields[col_indexes["condition"]],
+        "mileage": float(line_fields[col_indexes["mileage"]]),
+        "fuel_type": line_fields[col_indexes["fuel_type"]],
+        "volume": line_fields[col_indexes["volume"]],
+        "color": line_fields[col_indexes["color"]],
+        "transmission": line_fields[col_indexes["transmission"]],
+        "drive_unit": line_fields[col_indexes["drive_unit"]],
+        "segment": line_fields[col_indexes["segment"]]
+    }
+
+
+def standardize_empty_numeric_field(record, field_indexes):
+    """
+        Inserts "None" to the field if it's empty or has a "NaN" value
+    """
+    for i in field_indexes:
+        record[i] = float(record[i]) if record[i] != "" and record[i].lower() != "nan" else None
     return record
 
 
 if __name__ == "__main__":
+    db_user, db_pass, db_host = os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), os.getenv("DB_HOST")
+
+    if not db_user or not db_pass or not db_host:
+        print(db_user, db_pass, db_host)
+        raise Exception("No database credentials found!")
+
+    db_uri = f"mongodb+srv://{db_user}:{db_pass}@{db_host}"
+
     with beam.Pipeline() as pipeline:
+        print("Obtaining clean data")
         cars_data = (pipeline
             | "Read input" >> beam.io.ReadFromText('cars.csv')
             | "Filter out headers" >> beam.ParDo(FilterOutHeader('make'))
             | "Remove duplicates" >> beam.Distinct()
             | "Split by separator" >> beam.Map(lambda line: line.split(','))
-            | "Display expensive" >> beam.Filter(lambda record: int(record[PRICE_IDX]) > 100000)
-            | "De-hyphenate" >> beam.Map(dehyphenate)
-            | "Join data" >> beam.Map(lambda record: ','.join(record))
-            | "Output to file" >> beam.io.WriteToText('salida.txt'))
+            | "Convert to dict" >> beam.Map(convert_to_dict)
+            | "Filter expensive" >> beam.Filter(lambda record: int(record["price"]) >= 7000)
+            | "Clean volume data" >> beam.Map(standardize_empty_numeric_field, ["volume"])
+            | "De-hyphenate" >> beam.Map(dehyphenate))
+
+        print("Sinking cars with drive-unit ")
+        sink_valid = (cars_data
+            | "Filter cars with drive-unit" >> beam.Filter(lambda record: record["drive_unit"] != "")
+            | "Sink valid cars to MongoDB" >> WriteToMongoDB(uri=db_uri, db='carsdb', coll='valid_cars'))
+
+        print(sink_valid)
+
+        print("Sinking cars without drive-unit")
+        sink_invalid = (cars_data
+            | "Filter cars without drive-unit" >> beam.Filter(lambda record: record["drive_unit"] == "")
+            | "Sink invalid cars to MongoDB" >> WriteToMongoDB(uri=db_uri, db='carsdb', coll='invalid_cars'))
         
-        print(cars_data)
+        print(sink_invalid)
+
+    print("Finished")
